7f7a0ba14b68dba5cd12c285abc1032789dc09a3
Nguyen Phu Minh <53084840+nguyenphuminh@users.noreply.github.com>
2020-07-19 23:58:22 +0700

    Add files via upload

--- ML.bat
+++ ML.bat
@@ -1,42 +1,42 @@
 @echo off
 title Marble Launcher
-color 73
+color 57
 mode con cols=157 lines=35
 :menu
 cls
 echo.    =====================================================================================================================================================
-echo.    =                                                                Marble Launcher                                                                    =
-echo.    =                                                                                                                                                   =
-echo.    =                                                                                                                                                   =
-echo.    =                                                                                                                                                   =
-echo.    =                                                                                                                                                   =
-echo.    =                                                                                                                                                   =
-echo.    =                                                                                                                                                   =
-echo.    =                                                                                                                                                   =
-echo.    =                                                                                                                                                   =
-echo.    =                                                                                                                                                   =
-echo.    =                                                         1.Open Marble's Codeless Editor                                                           =
-echo.    =                                                                                                                                                   =
-echo.    =                                                                                                                                                   =
-echo.    =                                                          2.Open Marble's Engine Folder                                                            =
-echo.    =                                                                                                                                                   =
-echo.    =                                                                                                                                                   =
-echo.    =                                                                3.Run Examples                                                                     =
-echo.    =                                                                                                                                                   =
-echo.    =                                                                                                                                                   =
-echo.    =                                                                   4.Credit                                                                        =
-echo.    =                                                                                                                                                   =
-echo.    =                                                                                                                                                   =
-echo.    =                                                                                                                                                   =
-echo.    =                                                                                                                                                   =
-echo.    =                                                                                                                                                   =
-echo.    =                                                                                                                                                   =
-echo.    =                                                                                                                                                   =
-echo.    =                                                                                                                                                   =
-echo.    =                                                                                                                                                   =
-echo.    =                                                                                                                                                   =
-echo.    =                                                                                                                                                   =
-echo.    =                                                                                                                                                   =
+echo.    =                    =                                                                     +                                                        =
+echo.    =   Marble Launcher  =                    Codeless - Opensource - Free -Easy to use       +                                                         =
+echo.    =                    =                                                                   +                                                          =
+echo.    =++++++++++++++++++++=                                                                  +                                                           =
+echo.    =                    =                                                                 +      Wanna create games now? We got you covered!           =
+echo.    =                    =                                                                +                                                             =
+echo.    =                    ================================================================================================================================
+echo.    =                    =                                                                                                                              =
+echo.    =                    =                                                                                                                              =
+echo.    =                    =                                                                                                                              =
+echo.    =                    =                                                                                                                              =
+echo.    =                    =                                                                                                                              =
+echo.    =                    =                                          1.Open Marble's Codeless Editor                                                     =
+echo.    =                    =                                                                                                                              =
+echo.    =                    =                                                                                                                              =
+echo.    =                    =                                           2.Open Marble's Engine Folder                                                      =
+echo.    =                    =                                                                                                                              =
+echo.    =                    =                                                                                                                              =
+echo.    =                    =                                                  3.Run Examples                                                              =
+echo.    =                    =                                                                                                                              =
+echo.    =                    =                                                                                                                              =
+echo.    =                    =                                                     4.Credit                                                                 =
+echo.    =                    =                                                                                                                              =
+echo.    =                    =                                                                                                                              =
+echo.    =                    =                                                                                                                              =
+echo.    =                    =                                                                                                                              =
+echo.    =                    =                                                                                                                              =
+echo.    =                    =                                                                                                                              =
+echo.    =                    =                                                                                                                              =
+echo.    =                    =                                                                                                                              =
+echo.    =                    =                                                                                                                              =
+echo.    =                    =                                                                                                                              =
 echo.    =====================================================================================================================================================
 @CHOICE /C:1234 /N
 if errorlevel 4 goto credit
